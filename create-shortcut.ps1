191a239e431d616718ca0f4bac9c6c75e001a20d
ArturGilowski <artugilowski@gmail.com>
2026-02-24 13:13:47 +0100

    Fix syntax error with nested quotes in powershell shortcut arguments causing immediate exit

--- create-shortcut.ps1
+++ create-shortcut.ps1
@@ -5,7 +5,7 @@ $Shortcut = $WshShell.CreateShortcut("$DesktopPath\PDF Splitter.lnk")
 $batPath = Join-Path $PSScriptRoot "run-servers.bat"
 
 $Shortcut.TargetPath = "powershell.exe"
-$Shortcut.Arguments = "-WindowStyle Hidden -Command `'Start-Process cmd -ArgumentList `'/c `"`"$batPath`"`"`' -WindowStyle Hidden`'"
+$Shortcut.Arguments = "-WindowStyle Hidden -Command ""Start-Process cmd -ArgumentList '/c', '""$batPath""' -WindowStyle Hidden"""
 $Shortcut.WorkingDirectory = $PSScriptRoot
 
 # Use a built-in Windows icon 
